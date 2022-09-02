fb94f1da1a7517d8df761ea265e84250ca390562
EC2 Default User <ec2-user@ip-172-31-8-245.ap-northeast-1.compute.internal>
2022-09-02 16:16:06 +0000

    [update]09030115商品新規登録ページ作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,6 +2,10 @@ class Admin::ItemsController < ApplicationController
 
   def index
     @items = Item.all
+    @item = Item.new
+    @genre = Genre.find_by(@item.genre_id)
+    @item = Item.find_by(params[:id])
+
   end
 
   def new
@@ -9,6 +13,14 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      flash[:notice] = "新しい商品を登録しました。"
+      redirect_to admin_item_path
+    else
+      flash[:arlet] = "新しい商品の登録に失敗しました。"
+      render :new
+    end
   end
 
   def show
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
+  validates :name, uniqueness: true
+  validates :introduction, length: { maximum: 100 }
+  validates :price, presence: true
+  # validates :is_active, presence: true
+
 end
