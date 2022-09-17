10d617965a6e64bf786b06e30fda2e6c34b6621a
EC2 Default User <ec2-user@ip-172-31-8-245.ap-northeast-1.compute.internal>
2022-09-17 14:05:38 +0000

    [update]09172305マイページ作成完了/配送先一覧・登録画面作成中

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,13 +1,25 @@
 class Public::AddressesController < ApplicationController
   def index
+    @addresses = current_customer
+    @address = current_customer
   end
 
   def edit
+    @address = current_customer
   end
 
   def create
+    @address = Address.new
   end
 
   def update
+    @address = current_customer
+    if @address.update(current_customer)
+      flash[:notice] = "変更を保存しました。"
+      redirect_to addresses_path
+    else
+      flash[:arlet] = "変更の保存に失敗しました。"
+      render :edit
+    end
   end
 end
