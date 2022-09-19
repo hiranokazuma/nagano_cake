0eb0b792496ae3df37a12b58af99a99287d73168
EC2 Default User <ec2-user@ip-172-31-8-245.ap-northeast-1.compute.internal>
2022-09-19 14:45:06 +0000

    [update]09192344配送先登録/一覧画面完成

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,20 +1,29 @@
 class Public::AddressesController < ApplicationController
   def index
-    @addresses = current_customer
-    @address = current_customer
+    @addresses = Address.all
+    @address = Address.new
+    @customer = current_customer.id
   end
 
   def edit
-    @address = current_customer
+    @address = Address.find(params[:id])
   end
 
   def create
-    @address = Address.new
+    @address = Address.new(address_params)
+    if @address.save
+      flash[:notice] = "新しい配送先を登録しました。"
+      redirect_to addresses_path
+    else
+      flash[:arlet] = "新しい配送先の登録に失敗しました。"
+      @addresses = Address.all
+      render :index
+    end
   end
 
   def update
-    @address = current_customer
-    if @address.update(current_customer)
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
       flash[:notice] = "変更を保存しました。"
       redirect_to addresses_path
     else
@@ -22,4 +31,22 @@ class Public::AddressesController < ApplicationController
       render :edit
     end
   end
+
+  def destroy
+    @address = Address.find(params[:id])
+    if @address.destroy
+      flash[:notice] = "配送先を削除しました。"
+      redirect_to addresses_path
+    else
+      flash[:arlet] = "配送先の削除に失敗しました。"
+      render :index
+    end
+  end
+
+  private
+
+  def address_params
+   params.require(:address).permit(:customer_id, :address, :postal_code, :namespace)
+  end
+
 end
